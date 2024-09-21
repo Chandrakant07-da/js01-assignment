3ffac3becada141f5f8e52d6178684c5da588577
Chandrakant Dahare <ckant912@gmail.com>
2024-09-21 23:55:07 +0530

    Create react app for user-list

---
+++ user-list/src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch the list of users from the API
+    fetch('http://localhost:3001/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error('Error fetching users:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>User List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
